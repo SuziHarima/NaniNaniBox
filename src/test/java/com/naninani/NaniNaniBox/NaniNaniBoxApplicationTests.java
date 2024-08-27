b56bcbd08a603ef32970da8b661eb86ddb1b9cc0
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-08-27 18:46:03 -0300

    estrutura inicial SpringBoot

---
+++ src/test/java/com/naninani/NaniNaniBox/NaniNaniBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naninani.NaniNaniBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaniNaniBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
